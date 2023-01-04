8dd6e841fdef820e019467f9ec2322377878b56a
dlrjs2360 <lhg3670@naver.com>
2023-01-04 17:51:16 +0900

    슬라이딩 윈도우

---
+++ 12891/main.py
@@ -0,0 +1,41 @@
+import sys
+import os
+path = os.path.dirname(os.path.abspath(__file__))
+sys.stdin = open(path+'/input.txt', "r")
+
+
+from collections import Counter
+s, p = map(int,input().split())
+arr = input()
+contain = list(map(int,input().split()))
+word = ["A","C","G","T"]
+def getAns(check):
+    for i in range(4):
+        if check[i] < contain[i]:
+            return 0
+    else:
+        return 1
+
+ans = 0
+check = [0] * (4)
+start = 0
+end = start+p-1
+
+for x in arr[start:end + 1]:
+    if x in word:
+        check[word.index(x)] += 1
+
+while end < s:
+    ans += getAns(check)
+
+    if arr[start] in word:
+        check[word.index(arr[start])] -= 1
+    start += 1
+
+    end += 1
+    if end >= s:
+        break
+    if arr[end] in word:
+        check[word.index(arr[end])] += 1
+
+print(ans)
