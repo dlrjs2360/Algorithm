6a08cd1d3a9a738ad601f1a9a1802495fa0d2282
dlrjs2360 <lhg3670@naver.com>
2023-01-05 14:05:54 +0900

    슬라이딩 윈도우

--- 15565/main.py
+++ 15565/main.py
@@ -3,44 +3,9 @@ import os
 path = os.path.dirname(os.path.abspath(__file__))
 sys.stdin = open(path+'/input.txt', "r")
 
-from collections import deque
+
 n,k = map(int,input().split())
 arr = list(map(int,input().split()))
-
-count = 0 # 현재 남은 1의 갯수
-Q = deque()
-for i in range(n):
-    x = arr[i]
-    if x == 1:
-        count += 1
-        Q.append(i)
-
-ans = 1000001
-res = 0
-if count < k:
-    print(-1)
-else:
-    start = Q.popleft()
-    end = start
-    while start <= end <= n-1:
-        L = end - start + 1
-
-        if arr[end] == 1:
-            res += 1
-
-        #print(start,end, res)
-
-        if res >= k:
-            ans = min(ans,L)
-            res -= 1
-
-            count -= 1
-            if count < k:
-                break
-
-            start = Q.popleft()
-
-        end += 1
-
-    print(ans)
+idx = [i for i in range(n) if arr[i] == 1]
+print( min( idx[i+k-1]-idx[i]+1 for i in range(len(idx)-k+1) )  if len(idx) >= k else -1 )
 
