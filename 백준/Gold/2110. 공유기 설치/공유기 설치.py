21d6b7eb28595b18b1609c69f85de7b3dc0d5c59
HEE-GEON <lhg3670@naver.com>
2023-01-10 22:34:35 +0900

    [Gold IV] Title: 공유기 설치, Time: 260 ms, Memory: 117128 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -0,0 +1,24 @@
+n, c = map(int,input().split())
+house = [int(input()) for _ in range(n)]
+house.sort()
+
+answer = 0
+left, right = 1, house[-1]-house[0]
+while left <= right:
+    mid = (left + right) // 2
+
+    count = 1
+    current = house[0]
+    for i in range(1,len(house)):
+        if house[i] >= current + mid:
+            count += 1
+            current = house[i]
+
+    #print("left: ", str(left), "right: ", str(right), "mid: ", str(mid), "count: ",str(count))
+    if count < c:
+        right = mid - 1
+    else:
+        answer = max(answer, mid)
+        left = mid + 1
+
+print(answer)
