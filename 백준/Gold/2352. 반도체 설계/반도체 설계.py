2ccca83d37de06819b2757317b408241187f4489
HEE-GEON <lhg3670@naver.com>
2023-01-12 11:23:58 +0900

    [Gold II] Title: 반도체 설계, Time: 152 ms, Memory: 118468 KB -BaekjoonHub

--- 백준/Gold/2352. 반도체 설계/반도체 설계.py
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.py
@@ -1,25 +1,10 @@
-
-
+import bisect
 n = int(input())
-arr = list(map(int,input().split()))
-
-res = [arr[0]]
-for i in range(1,n):
-    x = arr[i]
-    if x > res[-1]:
-        res.append(x)
-        continue
-
-    left, right = 0, len(res)
-    idx = 0
-    while left <= right:
-        mid = (left+right) // 2
-        val = res[mid]
-        if val >= x:
-            right = mid - 1
-            idx = mid
-        else:
-            left = mid + 1
-    res[idx] = x
-
-print(len(res))
+q = []
+for i in map(int, input().split()):
+    s = bisect.bisect_left(q, i)
+    if s != len(q):
+        q[s] = i
+    else:
+        q += [i]
+print(len(q))
