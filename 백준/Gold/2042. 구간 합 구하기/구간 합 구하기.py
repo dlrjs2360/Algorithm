0ed40f43cde05e47f20e9044dfc39c525403ea5c
HEE-GEON <lhg3670@naver.com>
2023-01-28 14:11:45 +0900

    [Gold I] Title: 구간 합 구하기, Time: 1736 ms, Memory: 162504 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
@@ -1,81 +1,33 @@
-
-# 세그먼트 트리 초기화
-# start, end: 범위(구간), node: 세그먼트 트리에서 노드 번호
-def init(start, end, node):
-    # start와 end가 같다는 것은 leaf node임을 의미한다.
-    if start == end:
-        tree[node] = nums[start]
-        return
-
-    mid = (start + end) // 2
-    init(start, mid, node * 2)  # 왼쪽 자식 노드의 구간합
-    init(mid + 1, end, node * 2 + 1)  # 오른쪽 자식 노드의 구간합
-
-    # 왼쪽 자식 노드에 있는 구간합과 오른쪽 자식 노드에 있는 구간합을 더한 값을 저장
-    tree[node] = tree[node * 2] + tree[node * 2 + 1]
-
-
-# 세그먼트 트리의 구간합
-# L: 구하고자 하는 구간합의 왼쪽 구간
-# R: 구하고자 하는 구간합의 오른쪽 구간
-# node: 현재 노드
-# nodeLeft: 노드의 왼쪽 구간
-# nodeRight: 노드의 오른쪽 구간
-def sum(L, R, node, nodeLeft, nodeRight):
-    # 구하고자 하는 구간합의 구간 안에 현재 노드의 구간이 포함되지 않는다면
-    # 0을 반환한다.
-    if R < nodeLeft or nodeRight < L:
+def query(node, start, end, left, right):
+    if start <= left and right <= end:
+        return stree[node]
+    if right < start or end < left:
         return 0
+    return query(node*2,start,end,left,(left+right)//2) + query(node*2+1,start,end,(left+right)//2+1,right)
 
-    # 구하고자 하는 구간합의 구간 안에 현재 노드의 구간이 포함된다면
-    # 현재 노드의 값을 반환한다.
-    if L <= nodeLeft and nodeRight <= R:
-        return tree[node]
+def update(node, idx, val, left, right):
+    if left == right == idx:
+        stree[node] = val
+        return val
+    if left > idx or idx > right:
+        return stree[node]
+    stree[node] = update(node*2,idx,val,left,(left+right)//2) + update(node*2+1,idx,val,(left+right)//2+1,right)
+    return stree[node]
 
-    # 구간이 겹치는 경우에는 자식 노드에 대하여 sum 함수를 호출한다.
-    mid = (nodeLeft + nodeRight) // 2
-    return sum(L, R, node * 2, nodeLeft, mid) + sum(L, R, node * 2 + 1, mid + 1, nodeRight)
+def build(node,left,right):
+    if left == right:
+        stree[node] = arr[right]
+        return stree[node]
+    stree[node] = build(node*2,left,(left+right)//2) + build(node*2+1,(left+right)//2+1,right)
+    return stree[node]
 
-
-# 세그먼트 트리 업데이트
-# L: 노드의 왼쪽 구간
-# R: 노드의 오른쪽 구간
-# node: 현재 노드
-# idx: 바꿀 값의 인덱스
-# val: 바꿀 값
-def update(L, R, node, idx, val):
-    if L == R == idx:  # 단일 구간 업데이트
-        tree[node] = val
-        return
-
-    # 현재 노드의 구간에 idx가 포함되지 않을 경우
-    # 작업 없이 종료
-    if idx < L or R < idx:
-        return
-
-    mid = (L + R) // 2
-    update(L, mid, node * 2, idx, val)  # 왼쪽 자식 노드 업데이트
-    update(mid + 1, R, node * 2 + 1, idx, val)  # 오른쪽 자식 노드 업데이트
-    tree[node] = tree[node * 2] + tree[node * 2 + 1]  # 업데이트된 자식 노드들을 더해서 현재 노드의 값에 저장
-
-
-if __name__ == '__main__':
-    N, M, K = map(int, input().split())
-    nums = []
-    tree = [0 for _ in range(N * 4)]
-
-    for _ in range(N):
-        nums.append(int(input()))
-
-    init(0, N - 1, 1)
-
-    for _ in range(M + K):
-        a, b, c = map(int, input().split())
-
-        if a == 1:
-            b -= 1
-            update(0, N - 1, 1, b, c)
-        else:
-            b -= 1
-            c -= 1
-            print(sum(b, c, 1, 0, N - 1))
+n,m,k = map(int,input().split())
+arr = [int(input()) for _ in range(n)]
+stree = [0] * (n*4+1)
+build(1,0,n-1)
+for _ in range(m+k):
+    a,b,c = map(int,input().split())
+    if a == 1: # b번째 수를 c로 바꾸기
+        update(1,b-1,c,0,n-1)
+    if a == 2: # b부터 c까지의 합을 구하기
+        print(query(1,b-1,c-1,0,n-1))
