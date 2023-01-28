35a7972c60ee51a7843e9460a9c19fc37769bace
HEE-GEON <lhg3670@naver.com>
2023-01-28 14:17:54 +0900

    [Gold I] Title: 구간 합 구하기, Time: 784 ms, Memory: 159988 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
@@ -1,9 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+def merge(left_val, right_val):
+    return left_val + right_val
 def query(node, start, end, left, right):
     if start <= left and right <= end:
         return stree[node]
     if right < start or end < left:
         return 0
-    return query(node*2,start,end,left,(left+right)//2) + query(node*2+1,start,end,(left+right)//2+1,right)
+    return merge(query(node*2,start,end,left,(left+right)//2), query(node*2+1,start,end,(left+right)//2+1,right))
 
 def update(node, idx, val, left, right):
     if left == right == idx:
@@ -11,14 +16,14 @@ def update(node, idx, val, left, right):
         return val
     if left > idx or idx > right:
         return stree[node]
-    stree[node] = update(node*2,idx,val,left,(left+right)//2) + update(node*2+1,idx,val,(left+right)//2+1,right)
+    stree[node] = merge(update(node*2,idx,val,left,(left+right)//2), update(node*2+1,idx,val,(left+right)//2+1,right))
     return stree[node]
 
 def build(node,left,right):
     if left == right:
         stree[node] = arr[right]
         return stree[node]
-    stree[node] = build(node*2,left,(left+right)//2) + build(node*2+1,(left+right)//2+1,right)
+    stree[node] = merge(build(node*2,left,(left+right)//2), build(node*2+1,(left+right)//2+1,right))
     return stree[node]
 
 n,m,k = map(int,input().split())
