50aa6134097d58712be2d37614adc3ed5ab6597a
HEE-GEON <lhg3670@naver.com>
2023-01-30 14:06:11 +0900

    [Gold I] Title: 구간 합 구하기, Time: 712 ms, Memory: 164248 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
@@ -1,38 +1,28 @@
 import sys
 input = sys.stdin.readline
-
-def merge(left_val, right_val):
-    return left_val + right_val
-def query(node, start, end, left, right):
-    if start <= left and right <= end:
-        return stree[node]
-    if right < start or end < left:
-        return 0
-    return merge(query(node*2,start,end,left,(left+right)//2), query(node*2+1,start,end,(left+right)//2+1,right))
-
-def update(node, idx, val, left, right):
-    if left == right == idx:
-        stree[node] = val
-        return val
-    if left > idx or idx > right:
-        return stree[node]
-    stree[node] = merge(update(node*2,idx,val,left,(left+right)//2), update(node*2+1,idx,val,(left+right)//2+1,right))
-    return stree[node]
-
-def build(node,left,right):
-    if left == right:
-        stree[node] = arr[right]
-        return stree[node]
-    stree[node] = merge(build(node*2,left,(left+right)//2), build(node*2+1,(left+right)//2+1,right))
-    return stree[node]
+def update(idx, num):
+    while idx <= len(stree):
+        stree[idx] += num
+        idx += (idx & -idx)
+def query(idx):
+    ans = 0
+    while idx > 0:
+        ans += stree[idx]
+        idx -= (idx & -idx)
+    return ans
 
 n,m,k = map(int,input().split())
-arr = [int(input()) for _ in range(n)]
+arr = [0]+[int(input()) for _ in range(n)]
+
 stree = [0] * (n*4+1)
-build(1,0,n-1)
+for i in range(1,n+1):
+    update(i,arr[i])
+
 for _ in range(m+k):
     a,b,c = map(int,input().split())
-    if a == 1: # b번째 수를 c로 바꾸기
-        update(1,b-1,c,0,n-1)
-    if a == 2: # b부터 c까지의 합을 구하기
-        print(query(1,b-1,c-1,0,n-1))
+    if a == 1:
+        diff = c-arr[b]
+        update(b,diff)
+        arr[b] += diff
+    if a == 2:
+        print(query(c) - query(b-1))
