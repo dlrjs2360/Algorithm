e0c7afed2223181f5876b347315001b44dc8cf2f
HEE-GEON <lhg3670@naver.com>
2023-02-08 16:38:27 +0900

    [Gold V] Title: 입국심사, Time: 284 ms, Memory: 116156 KB -BaekjoonHub

---
+++ 백준/Gold/3079. 입국심사/입국심사.py
@@ -0,0 +1,17 @@
+n,m = map(int,input().split())
+arr = [int(input()) for _ in range(n)]
+
+answer = 0
+left, right = 0, int(1e9)*m
+while left <= right:
+    cnt = 0
+    mid = (left+right)//2
+    for x in arr:
+        cnt += mid // x
+    if cnt >= m:
+        answer = mid
+        right = mid - 1
+        continue
+    left = mid + 1
+
+print(answer)
