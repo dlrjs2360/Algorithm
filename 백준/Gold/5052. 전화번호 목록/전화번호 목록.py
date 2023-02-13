dae39b7cde8ba78ba2aa37cc7cb7743a436f2394
HEE-GEON <lhg3670@naver.com>
2023-02-13 19:41:54 +0900

    [Gold IV] Title: 전화번호 목록, Time: 168 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -1,8 +1,9 @@
+import sys
+input = sys.stdin.readline
 T = int(input())
 for test_case in range(T):
     n = int(input())
-    arr = [input() for _ in range(n)]
-    arr.sort()
+    arr = sorted([str(input().rstrip()) for _ in range(n)])
     for i in range(n-1):
         if arr[i] == arr[i+1][:len(arr[i])]:
             print("NO")
