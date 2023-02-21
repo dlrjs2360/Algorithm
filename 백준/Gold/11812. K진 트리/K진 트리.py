244775100b3ba74bb55f4c32065c1dd1f5ddbd02
HEE-GEON <lhg3670@naver.com>
2023-02-21 12:59:28 +0900

    [Gold III] Title: K진 트리, Time: 388 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/11812. K진 트리/K진 트리.py
+++ 백준/Gold/11812. K진 트리/K진 트리.py
@@ -1,28 +1,17 @@
 import sys
 input = sys.stdin.readline
 
-def getDepth(node):
-    i,cnt = 0,0
-    while 1:
-        cnt += k**i
-        if cnt >= node:
-            return i
-        i += 1
-
 def getParent(node):
     return (node-2)//k + 1
-
 def LCA(x,y):
-    if getDepth(x) != getDepth(y):
-        while getDepth(x) != getDepth(y):
-            if getDepth(x) > getDepth(y):
-                x = getParent(x)
-                continue
-            y = getParent(y)
+    d = 0
     while x != y:
-        x = getParent(x)
-        y = getParent(y)
-    return x
+        if x > y:
+            x = getParent(x)
+        else:
+            y = getParent(y)
+        d += 1
+    return d
 
 n,k,q = map(int,input().split())
 for _ in range(q):
@@ -30,4 +19,4 @@ for _ in range(q):
     if k == 1:
         print(abs(x-y))
         continue
-    print(getDepth(x) + getDepth(y) - 2 * getDepth(LCA(x,y)))
+    print(LCA(x,y))
