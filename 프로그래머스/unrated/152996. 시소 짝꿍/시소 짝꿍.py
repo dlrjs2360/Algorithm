8b3442b2e4157cf082571edcae16d4f2a5b2f7f8
HEE-GEON <lhg3670@naver.com>
2023-04-02 22:37:11 +0900

    [unrated] Title: 시소 짝꿍, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.py
+++ 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.py
@@ -1,10 +1,3 @@
-'''
-1. 같은 몸무게끼리는 Combination으로 n(n-1)//2
-2. 같은 몸무게 사람들끼리는 묶어서 명수 저장하기
-    - A몸무게와 B몸무게가 수평을 이루면 A*B를 정답에 더하기
-3. 크기순으로 정렬해서 내 뒤의 수들과만 비교하면 된다.
-'''
-
 from collections import defaultdict
 
 def solution(weights):
