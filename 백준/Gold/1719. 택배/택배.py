1339ba0a0b7ba5924f75a65e4eb7aed316fd4e1e
HEE-GEON <lhg3670@naver.com>
2023-04-10 19:40:20 +0900

    [Gold III] Title: 택배, Time: 360 ms, Memory: 117420 KB -BaekjoonHub

---
+++ 백준/Gold/1719. 택배/택배.py
@@ -0,0 +1,26 @@
+n,m = map(int,input().split())
+graph = [[1e9]*(n+1) for _ in range(n+1)]
+dp = [[str(x)]*(n+1) for x in range(n+1)]
+
+for _ in range(m):
+    a,b,c = map(int,input().split())
+    graph[a][b] = c
+    graph[b][a] = c
+    dp[a][b] = b
+    dp[b][a] = a
+
+for k in range(1,n+1):
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            if i == j:
+                continue
+            if graph[i][j] > graph[i][k]+graph[k][j]:
+                graph[i][j] = graph[i][k]+graph[k][j]
+                dp[i][j] = dp[i][k]
+
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        dp[i][i] = "-"
+
+for x in dp[1:]:
+    print(*x[1:])
