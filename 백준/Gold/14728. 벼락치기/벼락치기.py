37470f37f30e827baf3a96635fb1311bc01b6545
HEE-GEON <lhg3670@naver.com>
2023-04-12 01:48:11 +0900

    [Gold V] Title: 벼락치기, Time: 636 ms, Memory: 34872 KB -BaekjoonHub

---
+++ 백준/Gold/14728. 벼락치기/벼락치기.py
@@ -0,0 +1,10 @@
+n,t = map(int,input().split())
+chapter = sorted([list(map(int,input().split())) for _ in range(n)])
+study = {0:0}
+for w,v in chapter:
+    tmp = dict()
+    for dv,dw in study.items():
+        if (sumw := w + dw) < study.get((sumv := v + dv), t + 1):
+            tmp[sumv] = sumw
+    study.update(tmp)
+print(max(study.keys()))
