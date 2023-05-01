98ac0df9ba1d113314dcbf77e5213bea4a0f1338
HEE-GEON <lhg3670@naver.com>
2023-05-01 09:03:20 +0900

    [unrated] Title: 요격 시스템, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.py
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.py
@@ -1,11 +1,5 @@
-'''
-1. 현재 요격가능한 범위의 가장 짧은 범위를 저장 - a,b
-2. (a,b)와 겹치는 범위가 있는 것들을 모두 한번에 처리
-3. 정렬한 배열에서는 다음 미사일이 범위를 벗어나면 그 뒤의 미사일들도 범위를 벗어남.
-'''
-
 def solution(targets):
-    answer = 1
+    answer = 0
     targets.sort()
     end = targets[0][1]
     for s,e in targets[1:]:
@@ -15,4 +9,4 @@ def solution(targets):
         elif e <= end: # 포함된 경우
             if e < end:
                 end = e
-    return answer
+    return answer+1
