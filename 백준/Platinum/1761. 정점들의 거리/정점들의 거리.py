51b5efa9c9e1f235a80e7004a82681998a1e0c49
HEE-GEON <lhg3670@naver.com>
2023-05-12 10:48:44 +0900

    [Platinum V] Title: 정점들의 거리, Time: 1696 ms, Memory: 130972 KB -BaekjoonHub

--- 백준/Platinum/1761. 정점들의 거리/정점들의 거리.py
+++ 백준/Platinum/1761. 정점들의 거리/정점들의 거리.py
@@ -1,27 +1,6 @@
-from collections import defaultdict,deque
-from heapq import heappop,heappush
-n = int(input())
-MAX = float('inf')
-graph = defaultdict(list)
-for _ in range(n-1):
-    a,b,c = map(int,input().split())
-    graph[a].append((c,b))
-    graph[b].append((c,a))
 
-root = 1
-parent = [x for x in range(n+1)]
-dist = [MAX] * (n+1)
-dist[root] = 0
-depth = [0] * (n+1)
-heap = [(0,root)]
-while heap:
-    w,node = heappop(heap)
-    for nw,nextNode in graph[node]:
-        if dist[nextNode] > w+nw:
-            depth[nextNode] = depth[node]+1
-            dist[nextNode] = w+nw
-            heappush(heap,(w+nw,nextNode))
-            parent[nextNode] = node
+from collections import defaultdict
+from heapq import heappop,heappush
 
 def LCS(a,b):
     while (da := depth[a]) != (db:=depth[b]):
@@ -32,6 +11,32 @@ def LCS(a,b):
     while a != b:
         a,b = parent[a],parent[b]
     return a
+
+def DIJKSTRA(root):
+    parent = [x for x in range(n + 1)]
+    dist = [MAX] * (n + 1)
+    dist[root] = 0
+    depth = [0] * (n + 1)
+    heap = [(0, root)]
+    while heap:
+        w, node = heappop(heap)
+        for nw, nextNode in graph[node]:
+            if dist[nextNode] > w + nw:
+                depth[nextNode] = depth[node] + 1
+                dist[nextNode] = w + nw
+                heappush(heap, (w + nw, nextNode))
+                parent[nextNode] = node
+    return dist,parent,depth
+
+n = int(input())
+MAX = float('inf')
+graph = defaultdict(list)
+for _ in range(n-1):
+    a,b,c = map(int,input().split())
+    graph[a].append((c,b))
+    graph[b].append((c,a))
+
+dist,parent,depth = DIJKSTRA(1)
 
 m = int(input())
 for _ in range(m):
