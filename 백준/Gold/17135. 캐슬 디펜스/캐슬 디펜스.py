5e85879dc1cb4ef48b3063eaabf8a27fb7f755aa
HEE-GEON <lhg3670@naver.com>
2023-05-26 17:17:44 +0900

    [Gold III] Title: 캐슬 디펜스, Time: 288 ms, Memory: 118156 KB -BaekjoonHub

--- 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.py
+++ 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.py
@@ -1,56 +1,35 @@
-import sys
-input = sys.stdin.readline
 from collections import deque
 from itertools import combinations
 
-N, M, D = map(int, input().split())
+n,m,dist = map(int,input().split())
+graph = [list(map(int,input().split())) for _ in range(n)]
 
-matrix = []
-for _ in range(N):
-    line = list(map(int, input().split()))
-    matrix.append(line)
-
-dx = [0, -1, 0]
-dy = [-1, 0, 1]
-
-
-def kill(archor):
-    temp_matrix = [x[:] for x in matrix]
-
-    killed = [[0] * M for _ in range(N)]
+def attack(archor):
+    field = [x[:] for x in graph]
+    killed = [[0] * m for _ in range(n)]
     result = 0
-    #적군 움직이는 턴 (한칸씩 내려가는거를 for문을 반대로 돌리는 거로 생각)
-    for i in range(N-1, -1, -1):
-        #이번턴 죽는 애(궁수들이 한번에 공격하니까)
-        this_turn = []
-        #각 궁수별로 bfs 돌리면서 사정거리 안 제일 가까운 적군 찾기
+    for i in range(n - 1, -1, -1):
+        killedNow = []
         for ay in archor:
-            #첫 값은 궁수 바로 위 칸으로 넣어줌
             dq = deque([(i, ay, 1)])
             while dq:
                 x, y, d = dq.popleft()
-                if temp_matrix[x][y] == 1:
-                    this_turn.append((x, y))
+                if field[x][y] == 1:
+                    killedNow.append((x, y))
                     if killed[x][y] == 0:
                         killed[x][y] = 1
                         result += 1
                     break
-                if d < D:
-                    for di in range(3):
-                        nx = x + dx[di]
-                        ny = y + dy[di]
-                        if 0 <= nx < N and 0 <= ny < M:
-                            dq.append((nx, ny, d+1))
-        #한 턴에 공격한 애들 한번에 죽이기
-        for x, y in this_turn:
-            temp_matrix[x][y] = 0
-                
+                if d < dist:
+                    for nx,ny in [(x+dx[di],y+dy[di]) for di in range(3) if 0 <= x+dx[di] < n and 0 <= y+dy[di] < m]:
+                            dq.append((nx, ny, d + 1))
+        # 한 턴에 공격한 애들 한번에 죽이기
+        for x, y in killedNow:
+            field[x][y] = 0
     return result
 
 answer = 0
-
-archor_pos = list(combinations([i for i in range(M)], 3))
-for a in archor_pos:
-    answer = max(answer, kill(a))
-
+dx,dy = [0, -1, 0], [-1, 0, 1]
+for archers in combinations(range(m),3):
+    answer = max(answer,attack(archers))
 print(answer)
