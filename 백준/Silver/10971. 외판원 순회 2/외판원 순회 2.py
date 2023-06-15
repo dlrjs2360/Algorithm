30f52532bc939d8cf2f65a97c6e1d22e32b738d6
HEE-GEON <lhg3670@naver.com>
2023-06-15 10:29:51 +0900

    [Silver II] Title: 외판원 순회 2, Time: 1344 ms, Memory: 119108 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -1,16 +1,19 @@
-def DFS(start,node,total,visited):
+import sys
+input = sys.stdin.readline
+
+def visitAll(n):
+    return sum(2**x for x in range(n))
+def DFS(start,node,visit,total):
     global answer
-    if start == node and bin(visited).split("b")[1] == "1"*n:
-        answer = min(answer,total)
+    if node == start and visit == visitAll(n):
+        answer = min(answer, total)
         return
-    for i in range(n):
-        if visited & (1 << i) or W[node][i] == 0:
-            continue
-        DFS(start, i, total+W[node][i], visited | (1 << i))
+    for nextNode in range(n):
+        if not visit & (1 << nextNode) and dist[node-1][nextNode-1] > 0:
+            DFS(start,nextNode,visit | (1 << nextNode), total+dist[node-1][nextNode-1])
 
 n = int(input())
-W = [list(map(int,input().split())) for _ in range(n)]
+dist = [list(map(int,input().split())) for _ in range(n)]
 answer = 1e9
 DFS(1,1,0,0)
-
 print(answer)
