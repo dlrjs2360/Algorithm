cf978789a077ca18592c554f49687f572cbb69fb
HEE-GEON <lhg3670@naver.com>
2023-06-23 19:05:52 +0900

    [Gold V] Title: 거의 소수, Time: 3604 ms, Memory: 109380 KB -BaekjoonHub

--- 백준/Gold/1456. 거의 소수/거의 소수.py
+++ 백준/Gold/1456. 거의 소수/거의 소수.py
@@ -1,20 +1,19 @@
-m, n = map(int, input().split())
-
-prime = [True] * (int(n ** 0.5) + 1)
+A, B = map(int, input().split())
+rootB = int(B ** 0.5) + 1
+prime = [True] * rootB
 prime[1] = False
 
-for i in range(2, int(n ** 0.5) + 1):
-    if prime[i]:
-        for j in range(i**2, int(n ** 0.5) + 1, i):
-            prime[j] = False
+for i in range(2, rootB):
+    if not prime[i]: continue
+    for j in range(i**2, rootB, i): prime[j] = False
 
-count = 0
+answer = 0
 for i in range(1, len(prime)):
-    if prime[i]:
-        res = i * i
-        while 1:
-            if res > n: break
-            count += int(res >= m)
-            res *= i
+    if not prime[i]: continue
+    res = i * i
+    while 1:
+        if res > B: break
+        answer += int(res >= A)
+        res *= i
 
-print(count)
+print(answer)
