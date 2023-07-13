1b32a4e722067339377afec05a1c715f136483a5
HEE-GEON <lhg3670@naver.com>
2023-07-13 22:34:25 +0900

    [Gold III] Title: 다리 만들기, Time: 76 ms, Memory: 34340 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.py
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.py
@@ -1,65 +1,44 @@
 from collections import deque
 
-# 입력값
-n = int(input())
-graph = [list(map(int, input().split())) for _ in range(n)]
-
-visited = [[0] * n for _ in range(n)]
-num = 1
-res = int(1e9)
-
-
-# 1번째 bfs (섬 구분)
-def bfs(i, j):
-    que = deque()
-    que.append([i, j])
-    while que:
-        x, y = que.popleft()
-        for (h, w) in [[1, 0], [0, 1], [-1, 0], [0, -1]]:
-            dx, dy = x + h, y + w
-            if 0 <= dx < n and 0 <= dy < n and not visited[dx][dy] and graph[dx][dy]:
-                visited[dx][dy] = 1
-                graph[dx][dy] = num
-                que.append([dx, dy])
+def calc_bridge_size(a, b):
+    global result
+    for x1, y1 in islands[a]:
+        for x2, y2 in islands[b]:
+            result = min(result, (abs(y2 - y1) + abs(x2 - x1) - 1))
+            if result == 1:
+                return
 
 
-# 2번째 bfs (최단거리 구하기)
-def bfs2(v):
-    queue = deque()
-    dist = [[-1] * n for _ in range(n)]
-
-    for i in range(n):
-        for j in range(n):
-            if graph[i][j] == v:
-                dist[i][j] = 0
-                queue.append([i, j])
-
+def bfs(x, y):
+    queue = deque([(x, y)])
+    graph[x][y] = 2
+    island = [(x, y)]
     while queue:
         x, y = queue.popleft()
-        for (w, h) in [[1, 0], [0, 1], [-1, 0], [0, -1]]:
-            dx, dy = x + w, y + h
-            if 0 <= dx < n and 0 <= dy < n:
-                # 다른 섬 만났다! 연결됨!
-                if graph[dx][dy] and graph[dx][dy] != v:
-                    return dist[x][y]
-                # 물이고 아직 다리 없는 곳
-                elif (not graph[dx][dy]) and dist[dx][dy] == -1:
-                    dist[dx][dy] = dist[x][y] + 1
-                    queue.append([dx, dy])
-
-    return int(1e9)
+        for deltaX, deltaY in delta:
+            X = x + deltaX
+            Y = y + deltaY
+            if 0 <= X < N and 0 <= Y < N and graph[X][Y] == 1:
+                graph[X][Y] = 2
+                queue.append((X, Y))
+                island.append((X, Y))
+    return island
 
 
-# 섬 구분
-for i in range(n):
-    for j in range(n):
-        if graph[i][j] and not visited[i][j]:
-            visited[i][j] = 1
-            graph[i][j] = num
-            bfs(i, j)
-            num += 1
-
-# 최단거리 구하기
-for v in range(1, num):
-    res = min(res, bfs2(v))
-print(res)
+delta = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+N = int(input())
+graph = [list(map(int,input().split())) for _ in range(N)]
+islands = []
+for i in range(N):
+    for j in range(N):
+        if graph[i][j] == 1:
+            islands.append(bfs(i, j))
+result = 20000
+for i in range(len(islands) - 1):
+    for j in range(i + 1, len(islands)):
+        calc_bridge_size(i, j)
+        if result == 1:
+            break
+    if result == 1:
+        break
+print(result)
