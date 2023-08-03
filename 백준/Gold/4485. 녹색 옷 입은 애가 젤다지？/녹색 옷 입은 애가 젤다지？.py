d804e8fd933ad3b5075afedf2e552cd78d6e6949
HEE-GEON <lhg3670@naver.com>
2023-08-03 10:21:41 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 156 ms, Memory: 33324 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -1,6 +1,6 @@
 import sys
 input = sys.stdin.readline
-from collections import deque
+from heapq import heappop,heappush
 
 dx, dy = [1, 0, 0, -1], [0, 1, -1, 0]
 t = 1
@@ -8,15 +8,16 @@ while 1:
     N = int(input())
     if N == 0: break
     graph = [list(map(int, input().split())) for _ in range(N)]
-    queue = deque([(graph[0][0], 0, 0)])
+    heap = []
+    heappush(heap,(graph[0][0], 0, 0))
     dist = [[1e9] * N for _ in range(N)]
-    while queue:
-        w, x, y = queue.popleft()
+    while heap:
+        w, x, y = heappop(heap)
         if (x == N - 1 and y == N - 1) or dist[x][y] < w: continue
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
             if not (0 <= nx < N and 0 <= ny < N) or dist[nx][ny] <= (nw := w + graph[nx][ny]): continue
             dist[nx][ny] = nw
-            queue.append((nw, nx, ny))
+            heappush(heap,(nw, nx, ny))
     print(f"Problem {t}: {dist[N - 1][N - 1]}")
     t += 1
