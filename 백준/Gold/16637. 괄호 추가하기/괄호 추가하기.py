b9ac85fbe997bfa88f5725c3a1c772e271415b7c
HEE-GEON <lhg3670@naver.com>
2023-09-05 23:44:03 +0900

    [Gold III] Title: 괄호 추가하기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.py
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.py
@@ -1,31 +1,16 @@
+def calc(n1, op, n2):
+    if op == "+": return n1 + n2
+    if op == "-": return n1 - n2
+    if op == "*": return n1 * n2
+
+def dfs(depth, v):
+    global ans
+    if depth == N - 1: ans = max(ans, v); return
+    if depth + 2 < N:  dfs(depth + 2, calc(v, S[depth + 1], int(S[depth + 2])))
+    if depth + 4 < N: dfs(depth + 4, calc(v, S[depth + 1], calc(int(S[depth + 2]), S[depth + 3], int(S[depth + 4]))))
+
 N = int(input())
-data = list(map(lambda x: int(x) if x.isdigit() else x, input()))
-result = -int(1e9)
-
-
-def calculate(num1, num2, s):  # 연산자 s에 따라 계산처리
-    if s == '+':
-        return num1 + num2
-    elif s == '-':
-        return num1 - num2
-    elif s == '*':
-        return num1 * num2
-
-
-def dfs(idx, prev):
-    global result
-    if idx >= N:  # 연산이 끝났을 때
-        result = max(result, prev)  # 최댓값 갱신
-        return
-    if idx + 3 < N:  # 괄호 사용 가능
-        # 현재 연산자 위치 다음에 나오는 연산자에 대해 괄호로 묶어 계산
-        dfs(idx + 4, calculate(prev, calculate(data[idx + 1], data[idx + 3], data[idx + 2]), data[idx]))
-    dfs(idx + 2, calculate(prev, data[idx + 1], data[idx]))  # 괄호로 묶지 않고 계산
-
-
-if N == 1:  # 주어진 수가 하나일 때
-    result = data[0]
-else:
-    dfs(1, data[0])
-
-print(result)
+S = input()
+ans = -1e9
+dfs(0, int(S[0]))
+print(ans)
