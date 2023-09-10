1a683b4652fe05ab17dc8ac821675cab86915639
HEE-GEON <lhg3670@naver.com>
2023-09-10 19:04:35 +0900

    [Gold III] Title: 소수의 연속합, Time: 2012 ms, Memory: 92920 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
@@ -0,0 +1,25 @@
+
+n = int(input())
+
+# 에라토스테네스의 체
+a = [False, False] + [True] * (n - 1)
+prime = []
+for i in range(2, n +1):
+    if a[i]:
+        prime.append(i)
+        for j in range(2 * i, n + 1, i):
+            a[j] = False
+
+ans = 0
+start, end = 0, 0
+while end <= len(prime):
+    tmp = sum(prime[start:end])
+    if tmp == n:
+        ans += 1
+        end += 1
+    elif tmp < n:
+        end += 1
+    else:
+        start += 1
+
+print(ans)
