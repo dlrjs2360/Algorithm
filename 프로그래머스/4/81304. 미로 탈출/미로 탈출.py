a6305c4bea291e5f011fad9573808332aaaa8e99
HEE-GEON <lhg3670@naver.com>
2023-11-25 11:56:08 +0900

    [level 4] Title: 미로 탈출, Time: 29.78 ms, Memory: 19 MB -BaekjoonHub

--- 프로그래머스/4/81304. 미로 탈출/미로 탈출.py
+++ 프로그래머스/4/81304. 미로 탈출/미로 탈출.py
@@ -1,12 +1,19 @@
 import heapq
  
 def solution(n, start, end, roads, traps):
+    
+    def checkFlag(state, node):
+        return (state & (1 << traps_index[node])) > 0
+    
+    def activateTrap(state,node):
+        return state ^ (1 << traps_index[node])
+    
     answer = 0
     INF = float('inf')
-    dp = [[INF for _ in range(n+1)] for _ in range(1<<len(traps))]
-    traps_index ={value:ind  for ind,value in enumerate(traps) }
+    dp = [[INF] * (n+1) for _ in range(1 << len(traps))]
+    traps_index ={value:ind  for ind,value in enumerate(traps)}
     node_list = []
-    graph =[[] for _ in range(n+1)]
+    graph = [[] for _ in range(n+1)]
     
     for road in roads:
         x,y,pay = road
@@ -15,34 +22,35 @@ def solution(n, start, end, roads, traps):
     
     heapq.heappush(node_list,(0,start,0)) # (현재비용, 현재노드, 함정 상태)
     dp[0][start] = 0
+    
     while node_list:
         cur_time,cur_node,state = heapq.heappop(node_list)
+        
         if end == cur_node:
             answer = cur_time
             break
-        if dp[state][cur_node] < cur_time:
-            continue
-        for next_node,pay,flag in graph[cur_node]:
+            
+        if dp[state][cur_node] < cur_time: continue
+            
+        for next_node, pay, flag in graph[cur_node]:
             next_state = state
+            
             if cur_node in traps:
                 if next_node in traps:
-                    cur_flag = (1 & (state >> traps_index[cur_node]) + 
-                    (1 & (state >> traps_index[next_node]))) % 2
-                    next_state = state ^ (1 << traps_index[next_node])
+                    cur_flag = (checkFlag(state,cur_node) + checkFlag(state,next_node)) % 2
+                    next_state = activateTrap(state,next_node) # ^ : XOR(다르면 1)
                 else:
-                    cur_flag = 1 & (state >> traps_index[cur_node])
+                    cur_flag = checkFlag(state,cur_node)
             else:
                 if next_node in traps:
-                    cur_flag =  1 & (state >> traps_index[next_node])
-                    next_state = state ^ (1 << traps_index[next_node])
+                    cur_flag =  checkFlag(state,next_node)
+                    next_state = activateTrap(state,next_node)
                 else:
                     cur_flag = 0
             
             if cur_flag != flag or dp[next_state][next_node] <= cur_time + pay: continue
             
             dp[next_state][next_node] = cur_time + pay
-            if next_node in traps:
-                heapq.heappush(node_list, (dp[next_state][next_node], next_node, next_state))
-            else:
-                heapq.heappush(node_list, (dp[next_state][next_node], next_node, next_state))
+            heapq.heappush(node_list, (dp[next_state][next_node], next_node, next_state))
+            
     return answer
