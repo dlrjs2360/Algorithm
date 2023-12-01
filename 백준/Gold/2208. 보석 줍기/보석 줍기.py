aa258b3ea63609257ff1cf14ef27ee7a14af2e21
HEE-GEON <lhg3670@naver.com>
2023-12-01 14:40:20 +0900

    [Gold II] Title: 보석 줍기, Time: 96 ms, Memory: 34972 KB -BaekjoonHub

--- 백준/Gold/2208. 보석 줍기/보석 줍기.py
+++ 백준/Gold/2208. 보석 줍기/보석 줍기.py
@@ -2,18 +2,13 @@ import sys
 input = sys.stdin.readline
 def solution():
     N, M = map(int, input().split())
-    gem = [int(input()) for _ in range(N)]
-
-    prefix = [gem[0]]
-    for i in range(1, N): prefix.append(prefix[i - 1] + gem[i])
-
-    dp = [prefix[0]]
-    for i in range(1,N): dp.append(min(dp[i-1],prefix[i]))
-
-    answer = 0 if M > 1 else prefix[0]
-    for i in range(M,N):
-        answer = max(prefix[i] - min(0,dp[i-M]), answer)
-
-    return max(0,answer)
+    prefix = [0]*(N+1)
+    for i in range(1, N+1):
+        prefix[i] = prefix[i-1]+int(input())
+    res = minVal = 0
+    for i in range(M, N+1):
+        minVal = min(minVal,prefix[i-M])
+        res = max(res,prefix[i]-minVal)
+    return res
 
 print(solution())
