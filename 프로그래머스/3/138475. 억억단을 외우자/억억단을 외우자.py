3fbd11206a8725c2f26f0ba56b8bbaa4e89de77f
HEE-GEON <lhg3670@naver.com>
2023-12-16 09:49:00 +0900

    [level 3] Title: 억억단을 외우자, Time: 8668.94 ms, Memory: 91.4 MB -BaekjoonHub

--- 프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.py
+++ 프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.py
@@ -1,19 +1,14 @@
 def solution(e, starts):
-    dp_count = [0] * (e + 1)
-    for div in range(2, e + 1):
-        for jump in range(0, e + 1, div):
-            dp_count[jump] += 2
-
-    for root in range(1, int(e ** 0.5) + 1):
-        dp_count[root ** 2] += 1
-
-    dp_div = [0] * (e + 1)
-    max_count = 0
-    for idx in range(e, 0, -1):
-        if max_count <= dp_count[idx]:
-            max_count = dp_count[idx]
-            dp_div[idx] = idx
+    arr = [0]*(e+1)
+    ans = [0]*(e+1)
+    i = 2
+    for i in range(2,e+1):
+        for j in range(i,e+1,i):
+            arr[j] += 1
+    ans[e] = e
+    for i in reversed(range(0,e)):
+        if arr[i] >= arr[ans[i+1]]:
+            ans[i] = i
         else:
-            dp_div[idx] = dp_div[idx + 1]
-
-    return [dp_div[start] for start in starts]
+            ans[i] = ans[i+1]
+    return [ans[i] for i in starts]
